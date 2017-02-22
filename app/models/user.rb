9f78a6c6ea1aed6ab28a1d09f87328dfe58c593d
Tinkesh Kumar <umamaheshvarma@gmail.com>
2017-02-23 02:14:27 +0530

    referal gift

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,35 +16,35 @@ class User < ActiveRecord::Base
   REFERRAL_STEPS = [
     {
       'count' => 5,
-      'html' => 'Travel Gift <br>Group C',
+      'html' => 'Travel Themed Bottle Opener',
       'class' => 'two',
       'image' =>  ActionController::Base.helpers.asset_path(
         'refer/cream-tooltip@2x.gif')
     },
     {
       'count' => 10,
-      'html' => 'Travel Gift <br>Group B',
+      'html' => 'Universal Travel Charger',
       'class' => 'three',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/truman@2x.gif')
     },
     {
       'count' => 15,
-      'html' => 'Travel Gift A + <br> 15 Entries into the Miami Vacation',
+      'html' => 'Exclusive Luggage Tag + <br>15 Entries for Miami Travel Award',
       'class' => 'four',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/winston@2x.jpg')
     },
     {
       'count' => 20,
-      'html' => 'Luggage Scale + <br>20 entries into Miami vacation',
+      'html' => 'Digital Luggage Scale + <br>20 Entries into Miami Travel Award',
       'class' => 'five',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/blade-explain@2x.jpg')
     },
     {
       'count' => 35,
-      'html' => '1st Class airline tickets + Sleeping Mask',
+      'html' => 'Padded Sleeping Mask + <br>1st Class Airline Ticket for Two',
       'class' => 'six',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/image011.jpg')
