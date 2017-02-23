21ac4e8fbda43ccb5955eea056a8d99b464b3e11
Tinkesh Kumar <umamaheshvarma@gmail.com>
2017-02-23 10:38:27 +0530

    admin interface

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,8 @@
 # Export to CSV with the referrer_id
 ActiveAdmin.register User do
+
+  before_filter :skip_sidebar!, :only => :index
+  
   csv do
     column :id
     column :email
@@ -10,5 +13,31 @@ ActiveAdmin.register User do
   end
 
   actions :index, :show
+
+  index do
+    selectable_column
+    column :id
+    column :email
+    column 'Referrals Count' do |user|
+      user.referrals.count
+    end
+    column :referral_code
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :email
+      row :referral_code
+      row :referrals_count do
+      	user.referrals.count
+      end
+      row :referrer
+      row :created_at
+    end
+    active_admin_comments
+  end
 
 end
